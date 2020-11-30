c197f29fd7030e10d8cf20a9937d4cc49dbffd78
don <donovandall@icloud.com>
2020-11-30 11:34:06 +0000

    add src

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,20 @@ use rspotify::model::track::FullTrack;
 use rspotify::oauth2::{SpotifyClientCredentials, SpotifyOAuth};
 use rspotify::senum::{Country, IncludeExternal, SearchType};
 use rspotify::util::get_token;
+use rspotify::model::cud_result::CUDResult;
 
 mod tidal;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let import_playlist = "0nAA9SMaBFq9HuthWj1E6D";
+    let test_import_playlist = "0nAA9SMaBFq9HuthWj1E6D";
 
     // You can use any logger for debugging.
     pretty_env_logger::init();
 
     // The default credentials from the `.env` file will be used by default.
     let mut oauth = SpotifyOAuth::default()
-        .scope("user-follow-read user-follow-modify")
+        .scope("user-follow-read user-follow-modify playlist-modify-private user-library-modify")
         .build();
 
     match get_token(&mut oauth).await {
@@ -26,7 +27,7 @@ async fn main() -> Result<(), Error> {
             let tidal = tidal::get_tidal_from_file(String::from("./tidal-tracks-mixed.json")).await?; //TODO add buffered reading
             println!("Importing {} tracks", tidal.items.len());
 
-            println!("Getting spotifty credentials");
+            println!("Getting spotify credentials");
             let client_credential = SpotifyClientCredentials::default()
                 .token_info(token_info)
                 .build();
@@ -34,6 +35,8 @@ async fn main() -> Result<(), Error> {
             let spotify = Spotify::default()
                 .client_credentials_manager(client_credential)
                 .build();
+
+            let user = spotify.current_user().await.expect("Failed to get user");
 
 
             let queries: Vec<(String, String)> = tidal.items.iter()
@@ -64,7 +67,27 @@ async fn main() -> Result<(), Error> {
                                 }).collect::<Vec<&FullTrack>>();
                             match tracks.first() {
                                 None => println!("Could not find {} {}", artist, query),
-                                Some(value) => log::debug!("Found {} {:?}", query, value.uri)
+                                Some(value) => {
+                                    let uri = value.uri.clone();
+                                    log::debug!("Found {} {:?}", query, uri);
+
+                                    let playlists = spotify.current_user_playlists(10, 0).await;
+                                    log::trace!("{:?}", playlists);
+
+                                    let added = spotify.user_playlist_add_tracks(
+                                        user.id.as_str(),
+                                        test_import_playlist,
+                                        &[uri],
+                                        None
+                                    ).await;
+
+                                    match added {
+                                        Ok(result) => println!("Added {:?}", result),
+                                        Err(err) => println!("Failed to add because {}", err),
+                                    }
+
+
+                                }
                             }
 
                         }
