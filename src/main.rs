b2a507e4fa5494ab5fcfa1d2f6a5f61fb42fbe9c
Donovan Dall <donovandall@icloud.com>
2020-11-30 12:45:46 +0000

    token issues

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ mod tidal;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let test_import_playlist = "1MnoSgQYkCliMM7NxQ0kcj";
+    let test_import_playlist = "0T0Rdfh8bepjWcLAURRQ0U";
 
     // You can use any logger for debugging.
     pretty_env_logger::init();
 
     // The default credentials from the `.env` file will be used by default.
     let mut oauth = SpotifyOAuth::default()
-        .scope("user-follow-read user-follow-modify playlist-modify-private user-library-modify")
+        .scope("user-read-recently-played playlist-modify-public playlist-modify-private user-follow-read user-follow-modify playlist-modify-private user-library-modify user-library-read")
         .build();
 
     match get_token(&mut oauth).await {
@@ -107,61 +107,6 @@ async fn main() -> Result<(), Error> {
                     Err(err) => println!("Failed to add because {}", err),
                 }
             })
-            // let added = spotify.user_playlist_add_tracks(
-            //     user.id.as_str(),
-            //     test_import_playlist,
-            //     &track_uris,
-            //     None,
-            // ).await;
-
-
-
-            // for (artist, query) in queries {
-            //     let query = query.replace("(feat. ", "");
-            //     let query = query.replace(")", "");
-            //     if let Ok(find) = spotify.search(
-            //         query.as_str(),
-            //         SearchType::Track,
-            //         50,
-            //         0,
-            //         None,
-            //         None,
-            //     ).await {
-            //         match find {
-            //             SearchResult::Tracks(tracks) => {
-            //                 let tracks = tracks.items
-            //                     .iter()
-            //                     .filter(|track| {
-            //                         let artists = build_track_artists(track);
-            //                         artists.contains(&artist)
-            //                     }).collect::<Vec<&FullTrack>>();
-            //                 match tracks.first() {
-            //                     None => println!("Could not find {} {}", artist, query),
-            //                     Some(value) => {
-            //                         let uri = value.uri.clone();
-            //                         log::debug!("Found {} {:?}", query, uri);
-            //
-            //                         let playlists = spotify.current_user_playlists(10, 0).await;
-            //                         log::trace!("{:?}", playlists);
-            //
-            //                         let added = spotify.user_playlist_add_tracks(
-            //                             user.id.as_str(),
-            //                             test_import_playlist,
-            //                             &[uri],
-            //                             None,
-            //                         ).await;
-            //
-            //                         match added {
-            //                             Ok(result) => println!("Added {:?}", result),
-            //                             Err(err) => println!("Failed to add because {}", err),
-            //                         }
-            //                     }
-            //                 }
-            //             }
-            //             _ => {}
-            //         }
-            //     };
-            // }
         }
         None => log::error!("Authentication failed"),
     };
